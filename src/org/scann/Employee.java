de6251ddb2bc1fb7354e33a21cef1742da095bc4
nagarjunjeeva <nagarjunjeeva98@gmail.com>
2022-06-04 21:00:50 +0530

    Done

---
+++ src/org/scann/Employee.java
@@ -0,0 +1,30 @@
+package org.scann;
+
+import java.util.Scanner;
+
+public class Employee {
+		public static void main(String[] args) {
+			Scanner s=new Scanner(System.in);
+			System.out.println("Enter Employee Id");
+			int Id =s.nextInt();
+			System.out.println("Enter Employee Name");
+			String name = s.next();
+			System.out.println("Enter Employee email");
+			String email =s.next();
+			System.out.println("Enter Employee Phone Number");
+			long phoneno = s.nextLong();
+			System.out.println("Enter Employee Salary");
+			float salary = s.nextFloat();
+			System.out.println("Enter Employee Gender");
+			String gender = s.next();
+			System.out.println("Enter Employee City");
+			String city = s.next();
+			System.out.println("Id:"+Id);
+			System.out.println("Name:"+name);
+			System.out.println("EmailId:"+email);
+			System.out.println("Phone No:"+phoneno);
+			System.out.println("Salary:"+salary);
+			System.out.println("Gender:"+gender);
+			System.out.println("City:"+city);
+		}
+}
